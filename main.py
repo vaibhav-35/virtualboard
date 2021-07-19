96e5bd4c29d5b95f94556b1f00dcf6ba65703af0
vaibhav-35 <77327065+vaibhav-35@users.noreply.github.com>
2021-07-20 05:15:15 +0530

    Fixed function names

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 #for multiple cameras
@@ -9,14 +9,14 @@ CamFound = False
 
 while not CamFound:
     currIdx = 0
-    checkVidSource = cv2.VideoCapture(currIdx)
+    checkVidSource = cv.VideoCapture(currIdx)
 
     while True:
         ret, frame = checkVidSource.read()
 
-        cv2.imshow("CurrentSource, Press 's' to use this source. 'n' and 'p' to navigate between sources",frame)
+        cv.imshow("CurrentSource, Press 's' to use this source. 'n' and 'p' to navigate between sources",frame)
 
-        key = cv2.waitKey(1)
+        key = cv.waitKey(1)
 
         if key == ord('s'):
             cameraIdx = currIdx
@@ -39,46 +39,46 @@ while not CamFound:
 
     if CamFound:
         checkVidSource.release()
-        cv2.destroyAllWindows()
+        cv.destroyAllWindows()
         break
 
 
 
-cap=cv2.VideoCapture(cameraIdx)
+cap=cv.VideoCapture(cameraIdx)
                 
 def nothing(x):
     pass
 
 #Create trackbar to adjust HSV range
-cv2.namedWindow("trackbar")
-cv2.createTrackbar("L-H","trackbar",0,179,nothing)
-cv2.createTrackbar("L-S","trackbar",0,255,nothing)
-cv2.createTrackbar("L-V","trackbar",0,255,nothing)
-cv2.createTrackbar("U-H","trackbar",179,179,nothing)
-cv2.createTrackbar("U-S","trackbar",255,255,nothing)
-cv2.createTrackbar("U-V","trackbar",255,255,nothing)
+cv.namedWindow("trackbar")
+cv.createTrackbar("L-H","trackbar",0,179,nothing)
+cv.createTrackbar("L-S","trackbar",0,255,nothing)
+cv.createTrackbar("L-V","trackbar",0,255,nothing)
+cv.createTrackbar("U-H","trackbar",179,179,nothing)
+cv.createTrackbar("U-S","trackbar",255,255,nothing)
+cv.createTrackbar("U-V","trackbar",255,255,nothing)
 
 while True:
     ret,frame =cap.read()
-    cv2.putText(frame, 'Adjust sliders till you only see pointer, Press "s" to save', (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2, cv2.LINE_4)
+    cv.putText(frame, 'Adjust sliders till you only see pointer, Press "s" to save', (50, 50), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2, cv.LINE_4)
 
-    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)    
+    hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)    
     
-    l_h=cv2.getTrackbarPos("L-H","trackbar")
-    l_s=cv2.getTrackbarPos("L-S","trackbar")
-    l_v=cv2.getTrackbarPos("L-V","trackbar")
-    h_h=cv2.getTrackbarPos("U-H","trackbar")
-    h_s=cv2.getTrackbarPos("U-S","trackbar")
-    h_v=cv2.getTrackbarPos("U-V","trackbar")
+    l_h=cv.getTrackbarPos("L-H","trackbar")
+    l_s=cv.getTrackbarPos("L-S","trackbar")
+    l_v=cv.getTrackbarPos("L-V","trackbar")
+    h_h=cv.getTrackbarPos("U-H","trackbar")
+    h_s=cv.getTrackbarPos("U-S","trackbar")
+    h_v=cv.getTrackbarPos("U-V","trackbar")
    
     low=np.array([l_h,l_s,l_v])
     high=np.array([h_h,h_s,h_v])
 
-    mask=cv2.inRange(hsv,low,high) 
-    result=cv2.bitwise_and(frame,frame,mask=mask)    
-    cv2.imshow("result",result)# If the user presses ESC then exit the program
+    mask=cv.inRange(hsv,low,high) 
+    result=cv.bitwise_and(frame,frame,mask=mask)    
+    cv.imshow("result",result)# If the user presses ESC then exit the program
     
-    key = cv2.waitKey(1)
+    key = cv.waitKey(1)
     # If the user presses `s` then print and save this array.
     if key == ord('s'):
         thearray = [[l_h,l_s,l_v],[h_h, h_s, h_v]]
@@ -90,13 +90,13 @@ while True:
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
 
 class drawingCanvas():
     def __init__(self):
         self.penrange = np.load('penrange.npy')
-        self.cap = cv2.VideoCapture(cameraIdx)
+        self.cap = cv.VideoCapture(cameraIdx)
         self.canvas = None
          
         self.x1,self.y1=0,0
@@ -107,7 +107,7 @@ class drawingCanvas():
     def draw(self):
         while True:
             _, self.frame = self.cap.read()
-            self.frame = cv2.flip( self.frame, 1 )
+            self.frame = cv.flip( self.frame, 1 )
 
             if self.canvas is None:
                 self.canvas = np.zeros_like(self.frame)
@@ -116,35 +116,35 @@ class drawingCanvas():
             contours=self.ContourDetect(mask)
             self.drawLine(contours)
             self.display()
-            k = cv2.waitKey(1) & 0xFF
+            k = cv.waitKey(1) & 0xFF
             self.takeAction(k)
             
             #if esc key is pressed exit
             if k == 27:
                 break        
     def CreateMask(self):
-        hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV) 
+        hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV) 
         lower_range = self.penrange[0]
         upper_range = self.penrange[1]
-        mask = cv2.inRange(hsv, lower_range, upper_range)
+        mask = cv.inRange(hsv, lower_range, upper_range)
         return mask
     
     def ContourDetect(self,mask):
         # Find Contours
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         return contours
     
     def drawLine(self,contours):
         #if contour area is not none and is greater than 100 draw the line
-        if contours and cv2.contourArea(max(contours, key = cv2.contourArea)) > 100:                
-            c = max(contours, key = cv2.contourArea)    
-            x2,y2,w,h = cv2.boundingRect(c)
+        if contours and cv.contourArea(max(contours, key = cv.contourArea)) > 100:                
+            c = max(contours, key = cv.contourArea)    
+            x2,y2,w,h = cv.boundingRect(c)
     
             if self.x1 == 0 and self.y1 == 0:
                 self.x1,self.y1= x2,y2
             else:
                 # Draw the line on the canvas
-                self.canvas = cv2.line(self.canvas, (self.x1,self.y1),(x2,y2), [255*self.val,0,0], 10)
+                self.canvas = cv.line(self.canvas, (self.x1,self.y1),(x2,y2), [255*self.val,0,0], 10)
             #New point becomes the previous point 
             self.x1,self.y1= x2,y2
         else:
@@ -153,9 +153,9 @@ class drawingCanvas():
     
     def display(self):
         # Merge the canvas and the frame.
-        self.frame = cv2.add(self.frame,self.canvas)    
-        cv2.imshow('Esc to close, "c" to clear screem, "e" to change modes',self.frame)
-        cv2.imshow('canvas',self.canvas)
+        self.frame = cv.add(self.frame,self.canvas)    
+        cv.imshow('Esc to close, "c" to clear screem, "e" to change modes',self.frame)
+        cv.imshow('canvas',self.canvas)
     
     def takeAction(self,k):
         # When c is pressed clear the entire canvas
@@ -168,4 +168,4 @@ class drawingCanvas():
 if __name__ == '__main__':
     drawingCanvas()
     
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
